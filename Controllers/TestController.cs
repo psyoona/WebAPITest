f55cbcc607fe66391caf066183661899074bdcf5
Seyoon Park <psyoona@naver.com>
2022-06-28 23:16:18 +0900

    Add TestController - SaveData api

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+using Newtonsoft.Json;
+
+namespace WebAPITest.Controllers
+{
+	public class TestController : ApiController
+	{
+		[HttpGet]
+		[Route("test")]
+		public string SaveData()
+		{
+			return JsonConvert.SerializeObject(new { Test = "test" });
+		}
+	}
+}
